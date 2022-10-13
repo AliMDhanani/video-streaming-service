5f6360db108df3b72ed9222c637a3573be24ebef
AliMDhanani <alidhanani2000@gmail.com>
2022-10-13 18:24:40 +0100

    initial commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA9EF-hH5MbeymHE0hZXqYEaGNQd9QuUR0",
+    authDomain: "video-streaming-service-a6f89.firebaseapp.com",
+    projectId: "video-streaming-service-a6f89",
+    storageBucket: "video-streaming-service-a6f89.appspot.com",
+    messagingSenderId: "100752125125",
+    appId: "1:100752125125:web:a630a4052081d90f5854ac"
+  };
