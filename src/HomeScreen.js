a48d308ba661a1dc84d8df73372dfdd9e0cbdc0f
AliMDhanani <alidhanani2000@gmail.com>
2022-10-13 18:24:22 +0100

    initial commit

---
+++ src/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./HomeScreen.css";
+
+const HomeScreen = () => {
+  return <div className="homeScreen">
+    {/* NAVBAR */}
+    
+    {/* BANNER */}
+
+    {/* ROW */}
+
+  </div>;
+};
+
+export default HomeScreen;
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nav.css';
+
+const Nav = () => {
+  return (f
+    <div className='nav'>
+        Nav
+    </div>
+  )
+}
+
+export default Nav
